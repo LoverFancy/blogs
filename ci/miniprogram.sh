81eec5a55820f3e659012c61905ed0dfae7e05d5
ganzhanjian <ganzhanjian@mucfc.com>
2020-04-20 20:59:28 +0800

    docs: update

--- ci/miniprogram.sh
+++ ci/miniprogram.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-/Applications/wechatwebdevtools.app/Contents/MacOS/cli preview --project /Users/jackiegan/TestProject/taro_optimize_webpack/dist/
+# /Applications/wechatwebdevtools.app/Contents/MacOS/cli preview --project /Users/jackiegan/TestProject/taro_optimize_webpack/dist/
+
+alias wecli='/c/Program\ Files\ \(x86\)/Tencent/微信web开发者工具/cli.bat'
+
+ret = wecli upload --project /d/TestProject/madp_test_leave --appid wx0df06037fa379914 -v 0.1.1 -d 'test'
+
+if [ ret ] echo 'result is ${ret}'
+
 
 
